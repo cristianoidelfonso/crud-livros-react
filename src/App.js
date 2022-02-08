d3bf8943b4123624fa8b039745119cab16ea5034
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-02-08 16:09:48 -0300

    Utilizando localStorage para armazenamento

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SimpleStorage from 'react-simple-storage';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Menu from "./components/Menu";
 import TableBooks from "./components/TableBooks";
@@ -8,28 +9,29 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    livros: [
-      {
-        id: 1,
-        isbn: "978-85-7522-403-8",
-        titulo: "HTML5 - 2ª Edição",
-        autor: "Maurício Samy Silva",
-      },
-      {
-        id: 2,
-        isbn: "978-85-7522-807-4",
-        titulo: "Introdução ao Pentest",
-        autor: "Daniel Moreno",
-      },
-      {
-        id: 3,
-        isbn: "978-85-7522-780-8",
-        titulo: "Internet das Coisas para Desenvolvedores",
-        autor: "Ricardo da Silva Ogliari",
-      },
-    ],
-  };
+  state = { livros:[] }
+  // state1 = {
+  //   livros: [
+  //     {
+  //       id: 1,
+  //       isbn: "978-85-7522-403-8",
+  //       titulo: "HTML5 - 2ª Edição",
+  //       autor: "Maurício Samy Silva",
+  //     },
+  //     {
+  //       id: 2,
+  //       isbn: "978-85-7522-807-4",
+  //       titulo: "Introdução ao Pentest",
+  //       autor: "Daniel Moreno",
+  //     },
+  //     {
+  //       id: 3,
+  //       isbn: "978-85-7522-780-8",
+  //       titulo: "Internet das Coisas para Desenvolvedores",
+  //       autor: "Ricardo da Silva Ogliari",
+  //     },
+  //   ],
+  // };
 
   inserirLivro = livro => {
     livro.id = this.state.livros.length + 1;
@@ -59,11 +61,15 @@ class App extends Component {
   render() {
     return (
       <Router>
+
+        <SimpleStorage parent = {this} />
+
         <Menu />
+        
         <Switch>
           <Route 
             exact 
-            path="/" 
+            path = "/" 
             render={() => 
               <TableBooks 
                 livros = {this.state.livros}
@@ -73,8 +79,8 @@ class App extends Component {
           />
           <Route 
             exact 
-            path="/cadastrar" 
-            render={() => (
+            path = "/cadastrar" 
+            render = {() => (
               <CreateBook
                 inserirLivro = { this.inserirLivro }
                 livro = {{ id:0, isbn:"", titulo:"", autor:"" }} 
@@ -83,8 +89,8 @@ class App extends Component {
           />
           <Route
             exact
-            path="/editar/:isbnLivro"
-            render={ props => {
+            path = "/editar/:isbnLivro"
+            render = { props => {
                 const livro = this.state.livros.find(
                   livro => livro.isbn === props.match.params.isbnLivro
                 );
